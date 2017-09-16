ba2b21988f2dab10deb813c8f299546996ae3b57
cyanide4all <marvazto@gmail.com>
2017-09-16 23:24:44 +0200

    Lista de usuarios agregados. Lista de roles al finalizar la partida

--- app/src/main/java/cyanoboru/secrethitler/add_player.java
+++ app/src/main/java/cyanoboru/secrethitler/add_player.java
@@ -1,13 +1,24 @@
 package cyanoboru.secrethitler;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import cyanoboru.secrethitler.core.Jugador;
 import cyanoboru.secrethitler.core.Partida;
 
 public class add_player extends AppCompatActivity {
@@ -17,9 +28,13 @@ public class add_player extends AppCompatActivity {
     EditText player_name;
     String player;
     Partida partida;
+    ListView listView;
 
     protected void addPlayer( String name ){
         partida.addPlayer(name);
+        ((add_playerAdapter)listView.getAdapter()).addData(name);
+        ((add_playerAdapter)listView.getAdapter()).notifyDataSetChanged();
+
         if(partida.getJugadores().size()>9){
             add_more.setVisibility(View.GONE);
         }
@@ -40,7 +55,10 @@ public class add_player extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
 
-        add_more = (Button) findViewById(R.id.addMorePlayers);
+        listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(new add_playerAdapter(this));
+
+                add_more = (Button) findViewById(R.id.addMorePlayers);
         no_more = (Button) findViewById(R.id.noMorePLayers);
         player_name = (EditText) findViewById(R.id.newPlayerName);
 
@@ -58,11 +76,54 @@ public class add_player extends AppCompatActivity {
             public void onClick(View v) {
                 String p = player_name.getText().toString();
                 addPlayer(p);
-                Toast.makeText(add_player.this, p+" se ha añadido a la partida", Toast.LENGTH_SHORT).show();
                 player_name.setText("");
             }
         });
 
         no_more.setVisibility(View.GONE);
+    }
+}
+
+class add_playerAdapter extends BaseAdapter{
+
+    private ArrayList<String> jugadores;
+    private Context context;
+
+    public add_playerAdapter(Context ctx){
+        this.context = ctx;
+        jugadores = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return jugadores.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return jugadores.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return jugadores.get(i).hashCode();
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TextView tv = new TextView(context);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tv.setTextAppearance(android.R.style.TextAppearance_Large);
+        }
+        tv.setText(jugadores.get(i));
+        return tv;
+    }
+
+    public void addData(String jugador){
+        jugadores.add(jugador);
+    }
+
+    public void deletePosition(int position){
+        jugadores.remove(position);
     }
 }
--- app/src/main/java/cyanoboru/secrethitler/vitoria.java
+++ app/src/main/java/cyanoboru/secrethitler/vitoria.java
@@ -3,12 +3,19 @@ package cyanoboru.secrethitler;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import cyanoboru.secrethitler.core.Jugador;
 import cyanoboru.secrethitler.core.Partida;
 
 public class vitoria extends AppCompatActivity implements View.OnClickListener {
@@ -37,6 +44,9 @@ public class vitoria extends AppCompatActivity implements View.OnClickListener {
         Button reiniciar = (Button) findViewById(R.id.reiniciarPartida);
         reiniciar.setOnClickListener(this);
 
+        ListView listView = (ListView) findViewById(R.id.listaView);
+        listView.setAdapter(new VictoriaAdapter(this));
+
         partidasGanadasView = (TextView) findViewById(R.id.PartidasGanadasView);
         partidasGanadasView.setText("Los liberales han ganado "+liberalesGanadas+" veces hasta hoy\n" +
                                     "Los fascistas han ganado "+fascistasGanadas+" veces hasta hoy");
@@ -60,6 +70,40 @@ public class vitoria extends AppCompatActivity implements View.OnClickListener {
         }
         editor.commit();
     }
+}
 
+class VictoriaAdapter extends BaseAdapter{
 
+    ArrayList<Jugador> jugadores;
+    Context ctx;
+
+    public VictoriaAdapter(Context ctx){
+        this.ctx = ctx;
+        jugadores = Partida.getInstance().getJugadores();
+    }
+
+    @Override
+    public int getCount() {
+        return jugadores.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return jugadores.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return jugadores.get(i).hashCode();
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        TextView tv = new TextView(ctx);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tv.setTextAppearance(android.R.style.TextAppearance_Large);
+        }
+        tv.setText(jugadores.get(i).getNombre()+ " : "+jugadores.get(i).getCartaDeIdentidad().getPersonaje());
+        return tv;
+    }
 }
